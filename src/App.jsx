26b651a827d7b8dcb67a035ce83de9592fa86945
DeusCorona <igor.njagul@gmail.com>
2023-12-18 06:13:31 +0100

    Added about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import NavBar from "./components/NavBar/NavBar";
 import CallToAction from "./components/CallToAction/CallToAction";
 import About from "./components/About/About";
-import Accordion from "./components/Accordion/Accordion";
-import Carousel from "./components/Carousel/Carousel";
 import Contact from "./components/Contact/Contact";
-import ESG from "./components/ESG/ESG";
 
 function App() {
 
   return (
     <>
       <CallToAction/>
+      <About title="Unikatnost nas razlikuje" text="Inovacija u svakom osmehu. - nije samo slogan, 
+      već način na koji pristupamo stomatologiji. Naša ordinacija je posebna jer koristimo najnovije 
+      tehnologije i inovacije u stomatologiji kako bismo pružili najbolju moguću negu našim pacijentima. 
+      Naš stručni tim je posvećen pružanju personalizovane usluge koja će zadovoljiti sve vaše dentalne potrebe. 
+      Naš cilj je da svaki pacijent napusti našu ordinaciju sa osmehom koji je rezultat vrhunske dentalne nege 
+      i inovativnih tretmana."/>
     </>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import ImplantImg from '../../assets/Implant.png'
 
-const About = () => {
+const About = (props) => {
     return (
-        <div>
-            
+        <div className='flex flex-col md:flex-row justify-between items-center gap-16 bg-charcoal text-white p-16 text-center md:text-left'>
+            <div className='flex flex-col items-left gap-4'>
+                <div className='text-3xl flex flex-col items-center md:items-start justify-center'>
+                    {props.title}
+                    <hr className='w-[21rem] h-[0.3rem] bg-white rounded'/>
+                </div>
+                <p>
+                    {props.text}
+                </p>
+            </div>
+            <div>
+                <img src={ImplantImg} alt="ImplantImg" />
+            </div>
+                        
         </div>
     );
 }
